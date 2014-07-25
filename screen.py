5a8f02fef80f99936713fa4696928c85448cc96b
satin <satin.jeet@gmail.com>
2014-07-25 15:13:43 +0530

    clicks

--- screen.py
+++ screen.py
@@ -3,6 +3,8 @@ import base64
 import cStringIO
 import os
 from BaseHTTPServer import BaseHTTPRequestHandler
+from pymouse import PyMouse
+from urlparse import urlparse
 
 class HTTPRequestHandler(BaseHTTPRequestHandler):
 
@@ -13,17 +15,30 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
             with open(path, 'r') as content_file:
                 content = content_file.read()
             ctype = "text/html"
+        elif self.path == "/jquery-2.js":
+            path = os.path.dirname(os.path.abspath(__file__))
+            path = path + "/jquery-2.js"
+            with open(path, 'r') as content_file:
+                content = content_file.read()
+            ctype = "text/javascript"
         else:
-            content = self.send_cast()
+            query = urlparse(self.path).query
+            content = self.send_cast(query)
             ctype = "text/plain"
         self.send_response(200)
         self.send_header('Content-Type', ctype)
         self.end_headers()
         self.wfile.write(content)
 
-    def send_cast(self):
+    def send_cast(self, query):
         w = gtk.gdk.get_default_root_window()
         sz = w.get_size()
+        if query is not '':
+            x,y,btn = query.split("&")
+            m = PyMouse()
+            x = int(sz[0] * float(x))
+            y = int(sz[1] * float(y))
+            m.click(x, y, int(btn))
         print "The size of the window is %d x %d" % sz
         pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB,False,8,sz[0],sz[1])
         pb = pb.get_from_drawable(w,w.get_colormap(),0,0,0,0,sz[0],sz[1])
